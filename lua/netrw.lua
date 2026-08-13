f830472aecc27b21eee143665923252d013a2d38
Ajani Small <159090147+AJANI2005@users.noreply.github.com>
2026-08-13 16:56:29 -0400

    Update netrw.lua

--- lua/netrw.lua
+++ lua/netrw.lua
@@ -6,3 +6,21 @@ vim.g.netrw_altv = 1
 vim.g.netrw_keepdir = 1
 
 vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
+
+local netrw_view
+
+vim.api.nvim_create_autocmd("BufWinLeave", {
+    callback = function()
+        if vim.bo.filetype == "netrw" then
+            netrw_view = vim.fn.winsaveview()
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    callback = function()
+        if vim.bo.filetype == "netrw" and netrw_view then
+            vim.fn.winrestview(netrw_view)
+        end
+    end,
+})
