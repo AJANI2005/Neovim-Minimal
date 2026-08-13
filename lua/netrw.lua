538fdd165d387d76c87acc7c8ddea308aeee0908
Ajani Small <159090147+AJANI2005@users.noreply.github.com>
2026-08-13 17:35:02 -0400

    Update netrw.lua

--- lua/netrw.lua
+++ lua/netrw.lua
@@ -6,3 +6,44 @@ vim.g.netrw_altv = 1
 vim.g.netrw_keepdir = 1
 
 vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
+
+
+local netrw_line
+
+vim.api.nvim_create_autocmd("WinLeave", {
+    callback = function()
+        local win = vim.api.nvim_get_current_win()
+        local buf = vim.api.nvim_win_get_buf(win)
+
+        if vim.bo[buf].filetype ~= "netrw" then
+            return
+        end
+
+        local row = vim.api.nvim_win_get_cursor(win)[1]
+        netrw_line = vim.api.nvim_buf_get_lines(buf, row - 1, row, false)[1]
+    end,
+})
+
+vim.api.nvim_create_autocmd("WinEnter", {
+    callback = function()
+        local win = vim.api.nvim_get_current_win()
+        local buf = vim.api.nvim_win_get_buf(win)
+
+        if vim.bo[buf].filetype ~= "netrw" or not netrw_line then
+            return
+        end
+
+        vim.defer_fn(function()
+            if not vim.api.nvim_win_is_valid(win) then
+                return
+            end
+
+            vim.api.nvim_set_current_win(win)
+
+            vim.fn.search(
+                "^" .. vim.fn.escape(netrw_line, [[\]]) .. "$",
+                "W"
+            )
+        end, 50)
+    end,
+})
