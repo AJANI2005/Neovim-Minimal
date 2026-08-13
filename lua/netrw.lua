29ea4628a6d88fbdcee7a5f9aaf6bc9f716a62ea
Ajani Small <159090147+AJANI2005@users.noreply.github.com>
2026-08-13 18:23:31 -0400

    Update netrw.lua

--- lua/netrw.lua
+++ lua/netrw.lua
@@ -5,34 +5,4 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_altv = 1
 vim.g.netrw_keepdir = 1
 
-vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
-
-
-local current_file
-
-vim.api.nvim_create_autocmd("WinLeave", {
-    callback = function()
-        local win = vim.api.nvim_get_current_win()
-        local buf = vim.api.nvim_win_get_buf(win)
-
-        if vim.bo[buf].filetype ~= "netrw" then
-            return
-        end
-
-        current_file = vim.fn.expand("%:t")
-    end,
-})
-
-vim.api.nvim_create_autocmd("WinEnter", {
-    callback = function()
-        local buf = vim.api.nvim_win_get_buf(0)
-
-        if vim.bo[buf].filetype ~= "netrw" or not current_file then
-            return
-        end
-
-        vim.schedule(function()
-            vim.fn.search("\\V" .. vim.fn.escape(current_file, "\\"), "W")
-        end)
-    end,
-})
+vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR")
