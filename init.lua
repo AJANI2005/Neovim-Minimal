3bbd2a6bb1c0730f36d19ddeb46dabe6a10bb328
Ajani Small <159090147+AJANI2005@users.noreply.github.com>
2026-08-13 16:46:05 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -45,7 +45,6 @@ vim.keymap.set("n", "<C-k>", "<cmd>wincmd k<CR>")
 vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>", { desc = "NetRW" })
 
 -- import modules in lua directory
 require("lsp")	   --language server protocol
