f8cd80907a0aabc7df02529b19a06131c6daf756
Ajani Small <ajani@cerebro.lan>
2026-08-19 23:00:24 -0400

    make background transparent

--- init.lua
+++ init.lua
@@ -99,4 +99,10 @@ require("netrw")       --netrw improvements
 require("statusline")  --custom statusline
 require("plugins")     --experimental plugins
 
+-- transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
 
